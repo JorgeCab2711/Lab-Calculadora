33f089e237ca07a15c777a1e6511d115f314b590
JorgeCab2711 <cab20009@uvg.edu.gt>
2022-05-05 14:59:30 -0600

    Basic structure

--- src/App.js
+++ src/App.js
@@ -2,45 +2,46 @@ import React from 'react';
 import './styles.scss';
 const App = () => (
   <>
-    <body>
-      <div className="all-container">
-        <header>Calculadora</header>
-        <div className="calculator-container">
-          {/*First row*/}
-          <div id="calculator-first-row"></div>
-          {/*second row*/}
-          <div className="calculator-row">
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>÷</button>
-          </div>
-          {/*Third row*/}
-          <div className="calculator-row">
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>*</button>
-          </div>
-          {/*Fourth row*/}
-          <div className="calculator-row">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>+</button>
-          </div>
-          {/*Fifth row*/}
-          <div className="calculator-row">
-            <button>.</button>
-            <button>0</button>
-            <button>=</button>
-            <button>-</button>
-          </div>
-          {/*Sixth row*/}
-          <button id="calculator-last-row">Clear</button>
+    <div className="all-container">
+      <header>Calculadora</header>
+      <div className="calculator-container">
+        {/*First row*/}
+        <div id="calculator-first-row">
+          <div className="previous-operator"></div>
+          <div className="current-operator"></div>
         </div>
+        {/*second row*/}
+        <div className="calculator-row">
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>÷</button>
+        </div>
+        {/*Third row*/}
+        <div className="calculator-row">
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>*</button>
+        </div>
+        {/*Fourth row*/}
+        <div className="calculator-row">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+        </div>
+        {/*Fifth row*/}
+        <div className="calculator-row">
+          <button>.</button>
+          <button>0</button>
+          <button>=</button>
+          <button>-</button>
+        </div>
+        {/*Sixth row*/}
+        <button id="calculator-last-row">Clear</button>
       </div>
-    </body>
+    </div>
   </>
 );
 
