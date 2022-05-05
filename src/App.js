4f9e930a1ff7d358a780c54d71702d4213f0f0dd
JorgeCab2711 <cab20009@uvg.edu.gt>
2022-05-05 12:42:53 -0600

    Estructura

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.scss';
+const App = () => (
+  <>
+    <body>
+      <div className="all-container">
+        <header>Calculadora</header>
+      </div>
+    </body>
+  </>
+);
+
+export default App;
