95600ec5c7825eeda2cba5e085467a49bbd21644
JorgeCab2711 <cab20009@uvg.edu.gt>
2022-05-18 20:00:47 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 
 function App() {
+  const [result, setResult] = useState('');
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
+
   return (
     <>
       <div className="all-container">
@@ -9,39 +15,68 @@ function App() {
         <div className="calculator-container">
           {/*First row*/}
           <div id="calculator-first-row">
-            <div className="previous-operator"></div>
-            <div className="current-operator"></div>
+            <input type="text" value={result} />
           </div>
           {/*second row*/}
           <div className="calculator-row">
-            <button id="7">7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>÷</button>
+            <button name="7" onClick={handleClick}>
+              7
+            </button>
+            <button name="8" onClick={handleClick}>
+              8
+            </button>
+            <button name="9" onClick={handleClick}>
+              9
+            </button>
+            <button name="/" onClick={handleClick}>
+              ÷
+            </button>
           </div>
           {/*Third row*/}
           <div className="calculator-row">
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>*</button>
+            <button name="4" onClick={handleClick}>
+              4
+            </button>
+            <button name="5" onClick={handleClick}>
+              5
+            </button>
+            <button name="6" onClick={handleClick}>
+              6
+            </button>
+            <button name="*" onClick={handleClick}>
+              *
+            </button>
           </div>
           {/*Fourth row*/}
           <div className="calculator-row">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>+</button>
+            <button name="1" onClick={handleClick}>
+              1
+            </button>
+            <button name="2" onClick={handleClick}>
+              2
+            </button>
+            <button name="3" onClick={handleClick}>
+              3
+            </button>
+            <button name="+" onClick={handleClick}>
+              +
+            </button>
           </div>
           {/*Fifth row*/}
           <div className="calculator-row">
-            <button>.</button>
-            <button>0</button>
-            <button>=</button>
-            <button>-</button>
+            <button name="." onClick={handleClick}>
+              .
+            </button>
+            <button name="0" onClick={handleClick}>
+              0
+            </button>
+            <button onClick={handleClick}>=</button>
+            <button name="-" onClick={handleClick}>
+              -
+            </button>
           </div>
           {/*Sixth row*/}
-          <button id="calculator-last-row">Clear</button>
+          <button className="calculator-last-row">Clear</button>
         </div>
       </div>
     </>
